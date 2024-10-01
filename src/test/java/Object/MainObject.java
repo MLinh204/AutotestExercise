5a1756bd8531656d113708a1b603acf8564a69ca
MLinh204 <130550163+MLinh204@users.noreply.github.com>
2024-10-01 21:25:01 +0700

    Fixed some issues

--- src/test/java/Object/MainObject.java
+++ src/test/java/Object/MainObject.java
@@ -1,9 +1,11 @@
 package Object;
 
+import Function.MainFunction;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -11,51 +13,149 @@ import java.time.Duration;
 public class MainObject {
     private WebDriver driver;
     private WebDriverWait wait;
+    private MainFunction function;
 
-    public MainObject(WebDriver driver){
+    public MainObject(WebDriver driver) {
         this.driver = driver;
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        this.function = new MainFunction(driver);
     }
 
+    // Home page methods
+    public boolean isHomePageVisible() {
+        return findNavBtnByText(" Home").isDisplayed();
+    }
 
-    //Home element
-    public WebElement findNavBtnByText(String text){
-        String xpath = String.format("//ul [@class = 'nav navbar-nav']//a[text()='%s']", text);
+    public void clickSignupLoginButton() {
+        findNavBtnByText(" Signup / Login").click();
+    }
+
+    public WebElement findNavBtnByText(String text) {
+        String xpath = String.format("//ul[@class='nav navbar-nav']//a[text()='%s']", text);
         return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
     }
 
-    //Signup/Login element
-    public WebElement getSignupForm(){
+    public String getLoggedInUserName() {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//li//a[i[@class='fa fa-user']]"))).getText();
+    }
+
+    public void deleteAccount() {
+        driver.findElement(By.xpath("//li//a[i[@class='fa fa-trash-o']]")).click();
+    }
+
+    // Signup/Login methods
+    public boolean isNewUserSignupVisible() {
+        return getSignupForm().findElement(By.xpath(".//h2")).getText().equals("New User Signup!");
+    }
+
+    public void performSignup(String name, String email) {
+        signupNameInput().sendKeys(name);
+        signupEmailInput().sendKeys(email);
+        signupBtn().click();
+    }
+
+    public WebElement getSignupForm() {
         return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='signup-form']")));
     }
-    public WebElement signupNameInput(){
+
+    private WebElement signupNameInput() {
         WebElement signupForm = getSignupForm();
         return signupForm.findElement(By.xpath(".//input[@name='name']"));
     }
-    public WebElement signupEmailInput(){
+
+    private WebElement signupEmailInput() {
         WebElement signupForm = getSignupForm();
         return signupForm.findElement(By.xpath(".//input[@name='email']"));
     }
-    public WebElement signupBtn(){
+
+    private WebElement signupBtn() {
         WebElement signupForm = getSignupForm();
         return signupForm.findElement(By.xpath(".//button"));
     }
-    public WebElement accountInformationTitle(){
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//b[text()='Enter Account Information']")));
+
+    public String getAccountInformationTitle() {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//b[text()='Enter Account Information']"))).getText();
     }
-    public WebElement addressInformationTitle(){
-        return driver.findElement(By.xpath("//b[text()='Enter Account Information']"));
+
+    public void fillAccountDetails(String name, String password, String title, String firstName, String lastName,
+                                   String company, String address1, String address2, String country,
+                                   String state, String city, String zipcode, String mobileNumber) {
+        selectTitleByName(title).click();
+        findAddressFieldById("name").clear();
+        findAddressFieldById("name").sendKeys(name);
+        findAddressFieldById("password").sendKeys(password);
+
+        selectDateOfBirth("1", "2", "3");  // Example values, adjust as needed
+
+        checkboxById("newsletter").click();
+        checkboxById("optin").click();
+
+        findAddressFieldById("first_name").sendKeys(firstName);
+        findAddressFieldById("last_name").sendKeys(lastName);
+        findAddressFieldById("company").sendKeys(company);
+        findAddressFieldById("address1").sendKeys(address1);
+        findAddressFieldById("address2").sendKeys(address2);
+
+        Select selectCountry = new Select(countryOption());
+        selectCountry.selectByValue(country);
+
+        findAddressFieldById("state").sendKeys(state);
+        findAddressFieldById("city").sendKeys(city);
+        findAddressFieldById("zipcode").sendKeys(zipcode);
+        findAddressFieldById("mobile_number").sendKeys(mobileNumber);
+
+        createAccountBtn().click();
     }
-    public WebElement selectTitleByName(String name){
-        String xpath = String.format("//div[@class='clearfix']//label[text()='%s']//input", name);
+
+    private void selectDateOfBirth(String day, String month, String year) {
+        new Select(dateOptionsById("days")).selectByValue(day);
+        new Select(dateOptionsById("months")).selectByValue(month);
+        new Select(dateOptionsById("years")).selectByValue(year);
+    }
+
+    public boolean isAccountCreatedVisible() {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//b[text()='Account Created!']"))).isDisplayed();
+    }
+
+    public void continueAfterAccountCreation() {
+        driver.findElement(By.xpath("//a[text()='Continue']")).click();
+    }
+
+    public boolean isAccountDeletedVisible() {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//b[text()='Account Deleted!']"))).isDisplayed();
+    }
+
+    private WebElement selectTitleByName(String index) {
+        String xpath = String.format("//input[@id='id_gender%s']", index);
         return driver.findElement(By.xpath(xpath));
     }
-    public WebElement dateOptionsById(String id){
-        String xpath = String.format("//select[@id = '%s']", id);
+
+    private WebElement dateOptionsById(String id) {
+        String xpath = String.format("//select[@id='%s']", id);
         return driver.findElement(By.xpath(xpath));
     }
-    public WebElement checkboxById(String id){
-        String xpath = String.format("//input[@id = '%s']", id);
+
+    private WebElement checkboxById(String id) {
+        String xpath = String.format("//input[@id='%s']", id);
         return driver.findElement(By.xpath(xpath));
+    }
+
+    private WebElement findAddressFieldById(String id) {
+        String xpath = String.format("//input[@id='%s']", id);
+        WebElement field = driver.findElement(By.xpath(xpath));
+        function.scrollIntoView(field);
+        return field;
+    }
+
+    private WebElement countryOption() {
+        WebElement dropdown = driver.findElement(By.xpath("//select[@id='country']"));
+        function.scrollIntoView(dropdown);
+        return dropdown;
+    }
+
+    private WebElement createAccountBtn() {
+        WebElement button = driver.findElement(By.xpath("//button[text()='Create Account']"));
+        function.scrollIntoView(button);
+        return button;
     }
 }
--- src/test/java/Test/Test.java
+++ src/test/java/Test/Test.java
@@ -2,79 +2,112 @@ package Test;
 
 import Config.ConfigProperties;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import Object.MainObject;
+import org.testng.asserts.SoftAssert;
 
 import java.time.Duration;
-
-import static org.testng.AssertJUnit.assertEquals;
-import static org.testng.AssertJUnit.assertTrue;
+import java.util.logging.Logger;
 
 public class Test {
     private WebDriver driver;
     private WebDriverWait wait;
     private MainObject mainObject;
+    private SoftAssert softAssert;
+    private static final Logger logger = Logger.getLogger(Test.class.getName());
+
+    private static final String TEST_NAME = "Test";
+    private static final String TEST_EMAIL = "Test7@a.com";
+    private static final String TEST_PASSWORD = "Test123";
 
     @BeforeClass
-    public void setup(){
+    public void setup() {
+        logger.info("Setting up the test environment");
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
-        this.driver = new ChromeDriver();
+        this.driver = new ChromeDriver(options);
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         this.mainObject = new MainObject(driver);
+        this.softAssert = new SoftAssert();
         driver.manage().window().maximize();
-    }
-    @AfterClass
-    public void close(){
-        if (driver!=null){
-            driver.quit();
-        }
-    }
-    //Test case 1
-    @org.testng.annotations.Test
-    public void TC1(){
-        //Step 1: Navigate to website
-        driver.get(ConfigProperties.BASE_URL);
-        //Step 2: Verify home page is visible
-        WebElement navHomeText = mainObject.findNavBtnByText(" Home");
-        assertTrue(navHomeText.isDisplayed());
-        //Step 3: Click Signup/Login button
-        WebElement signupLoginBtn = mainObject.findNavBtnByText(" Signup / Login");
-        signupLoginBtn.click();
-        //Step 4: Verify 'New User Signup!' is visible
-        WebElement signupForm = mainObject.getSignupForm();
-        assertEquals("New User Signup!", signupForm.findElement(By.xpath(".//h2")).getText());
-        //Step 5: Enter name and email address
-        mainObject.signupNameInput().sendKeys("Test");
-        mainObject.signupEmailInput().sendKeys("Test@a.com");
-        //Step 6: Click signup button
-        mainObject.signupBtn().click();
-        //Step 7: Verify that 'ENTER ACCOUNT INFORMATION' is visible
-        assertEquals("ENTER ACCOUNT INFORMATION", mainObject.accountInformationTitle().getText());
-        //Step 8: Fill details: Title, Name, Email, Password, Date of birth
-        mainObject.selectTitleByName("Mr. ").click();
-        WebElement nameField = driver.findElement(By.xpath("//input[@id = 'name']"));
-        nameField.clear();
-        nameField.sendKeys("Test");
-        WebElement passwordField = driver.findElement(By.xpath("//input[@id = 'password']"));
-        passwordField.sendKeys("Test123");
-        Select selectDay = new Select(mainObject.dateOptionsById("days"));
-        Select selectMonth = new Select(mainObject.dateOptionsById("months"));
-        Select selectYear = new Select(mainObject.dateOptionsById("years"));
-        selectDay.selectByIndex(1);
-        selectMonth.selectByIndex(2);
-        selectYear.selectByIndex(3);
-        //Step 9: Select checkbox 'Sign up for our newsletter!'
-        mainObject.checkboxById("newsletter").click();
+        logger.info("Test environment set up completed");
     }
 
+    @AfterClass
+    public void close() {
+        logger.info("Closing the test environment");
+        if (driver != null) {
+            driver.quit();
+        }
+        logger.info("Test environment closed");
+    }
+
+    @org.testng.annotations.Test
+    public void testUserRegistration() {
+        try {
+            logger.info("Starting user registration test");
+
+            // Step 1: Navigate to website
+            logger.info("Step 1: Navigating to website");
+            driver.get(ConfigProperties.BASE_URL);
+
+            // Step 2: Verify home page is visible
+            logger.info("Step 2: Verifying home page is visible");
+            softAssert.assertTrue(mainObject.isHomePageVisible(), "Home page should be visible");
+
+            // Step 3: Click Signup/Login button
+            logger.info("Step 3: Clicking Signup/Login button");
+            mainObject.clickSignupLoginButton();
+
+            // Step 4: Verify 'New User Signup!' is visible
+            logger.info("Step 4: Verifying 'New User Signup!' is visible");
+            softAssert.assertTrue(mainObject.isNewUserSignupVisible(), "'New User Signup!' should be visible");
+
+            // Step 5 & 6: Enter name and email address, then click signup
+            logger.info("Step 5 & 6: Entering name and email address, then clicking signup");
+            mainObject.performSignup(TEST_NAME, TEST_EMAIL);
+
+            // Step 7: Verify that 'ENTER ACCOUNT INFORMATION' is visible
+            logger.info("Step 7: Verifying 'ENTER ACCOUNT INFORMATION' is visible");
+            softAssert.assertEquals(mainObject.getAccountInformationTitle(), "ENTER ACCOUNT INFORMATION",
+                    "'ENTER ACCOUNT INFORMATION' should be visible");
+
+            // Step 8-12: Fill in account details and create account
+            logger.info("Step 8-12: Filling in account details and creating account");
+            mainObject.fillAccountDetails(TEST_NAME, TEST_PASSWORD, "1", "Johan", "Test", "Test Company",
+                    "Youtube, Facebook", "Instagram, Pinterest", "1", "Test state",
+                    "Test city", "123123", "5554443333");
+
+            // Step 13: Verify that 'ACCOUNT CREATED!' is visible
+            logger.info("Step 13: Verifying 'ACCOUNT CREATED!' is visible");
+            softAssert.assertTrue(mainObject.isAccountCreatedVisible(), "'ACCOUNT CREATED!' should be visible");
+
+            logger.info("Continuing after account creation");
+            mainObject.continueAfterAccountCreation();
+
+            // Step 14: Verify that 'Logged in as username' is visible
+            logger.info("Step 14: Verifying 'Logged in as username' is visible");
+            softAssert.assertEquals(mainObject.getLoggedInUserName(), "Logged in as " + TEST_NAME,
+                    "User should be logged in with correct name");
+
+            // Step 15: Click 'Delete Account' button
+            logger.info("Step 15: Clicking 'Delete Account' button");
+            mainObject.deleteAccount();
+
+            // Step 16: Verify that 'ACCOUNT DELETED!' is visible
+            logger.info("Step 16: Verifying 'ACCOUNT DELETED!' is visible");
+            softAssert.assertTrue(mainObject.isAccountDeletedVisible(), "'ACCOUNT DELETED!' should be visible");
+
+            logger.info("All test steps completed");
+            softAssert.assertAll();
+        } catch (Exception e) {
+            logger.severe("Test failed with exception: " + e.getMessage());
+            softAssert.fail("Test failed with exception: " + e.getMessage());
+        }
+    }
 }
